0f67548e7b1536bfcd88145f016faf6d52777231
Jona Kasten <jona.kasten@siemens.com>
2024-12-01 21:55:15 +0100

    day1 puzzle2

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -3,8 +3,9 @@
 #include <sstream>
 #include <algorithm>
 #include <numeric>
+#include <map>
 
-void solve(std::vector<std::string> lines) 
+void solve1(std::vector<std::string> lines) 
 {
     std::vector<int> firstList, secondList;
     int num1, num2;
@@ -25,25 +26,47 @@ void solve(std::vector<std::string> lines)
     {
         distances.push_back(abs(firstList[i] - secondList[i]));
     }
-
-    //for (int num1 : firstList) 
-    //{
-    //    std::cout << num1 << "\n";
-    //}
-
-    //std::cout << "======\n";
-
-    //for (int num2 : secondList) 
-    //{
-    //    std::cout << num2 << "\n";
-    //}
-
-    //for (int distance : distances)
-    //{
-    //    std::cout << distance << "\n";
-    //}
     int totalDistance = std::accumulate(distances.begin(), distances.end(), 0);
     std::cout << totalDistance << "\n";
+}
+
+void solve2(std::vector<std::string> lines) 
+{
+    std::vector<int> firstList, secondList;
+    int num1, num2;
+
+    for (std::string &line : lines)
+    {
+       std::istringstream ss(line);
+       ss >> num1 >> num2;
+       firstList.push_back(num1);
+       secondList.push_back(num2);
+    }
+
+    std::sort(firstList.begin(), firstList.end());
+    std::sort(secondList.begin(), secondList.end());
+
+    std::map<int,int> occFirst;
+    for (int firstNum : firstList)
+    {
+        occFirst[firstNum]++;
+    }
+
+    std::map<int,int> occSecond;
+    for (int secondNum : secondList)
+    {
+        occSecond[secondNum]++;
+    }
+
+    int simScore = 0;
+    for (auto &firstOcc : occFirst)
+    {
+        int occInSecondList = occSecond[firstOcc.first];
+        int increment = occInSecondList * firstOcc.first * firstOcc.second;
+        simScore += increment;
+    }
+
+    std::cout << simScore << "\n";
 }
 
 int main() 
@@ -52,7 +75,8 @@ int main()
     {
         std::vector<std::string> lines = readLinesFromFile("./day01/input.txt");
         // handle input
-        solve(lines);
+        //solve1(lines);
+        solve2(lines);
     } 
     catch (const std::exception &ex)
     {
