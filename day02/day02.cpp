550ff2ade0d76f3a5c70d12b18acb2db1ffd4589
Jona Kasten <jona.kasten@siemens.com>
2024-12-02 23:13:54 +0100

    day2 puzzle2

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -1,6 +1,48 @@
 #include "utils.h"
 #include<bits/stdc++.h>
 
+
+bool reportIsSafe(std::vector<int> report) 
+{
+    bool isSafe = true;
+    bool increase = report[1] > report[0];
+    for (size_t i = 1; i < report.size(); i++)
+    {
+        int current = report[i];
+        int prev = report[i-1];
+        if (current == prev)
+        {
+            isSafe = false;
+            break;
+        }
+        if (current > prev)
+        {
+            if (!increase) 
+            {
+                isSafe = false;
+                break;
+            }
+        }
+        else if (current < prev)
+        {
+            if(increase)
+            {
+                isSafe = false;
+                break;
+            }
+        }
+
+        int distance = abs(current - prev);
+        if (distance > 3)
+        {
+            isSafe = false;
+            break;
+        } 
+    }
+    return isSafe;
+}
+
+
 void solve1(std::vector<std::string> lines)
 {
     int safeSum = 0;
@@ -15,42 +57,55 @@ void solve1(std::vector<std::string> lines)
         {
             report.push_back(num);
         }
+        bool isSafe = reportIsSafe(report);
+        safeSum += isSafe? 1 : 0;
+    }
+    std::cout << safeSum << "\n";
+}
 
+bool checkSubsets(std::vector<int> report)
+{
+    int subset_size = report.size()-1;
+    std::vector<bool> bitmask(subset_size, true);
+    bitmask.resize(report.size(), false);
 
-        bool isSafe = true;
-        bool increase = report[1] > report[0];
-        for (size_t i = 1; i < report.size(); i++)
+    do 
+    {
+        std::vector<int> subset;
+        for (size_t i = 0; i < report.size(); i++)
         {
-            int current = report[i];
-            int prev = report[i-1];
-            if (current == prev)
+            if(bitmask[i])
             {
-                isSafe = false;
-                break;
+                subset.push_back(report[i]);
             }
-            if (current > prev)
-            {
-                if (!increase) 
-                {
-                    isSafe = false;
-                    break;
-                }
-            }
-            else if (current < prev)
-            {
-                if(increase)
-                {
-                    isSafe = false;
-                    break;
-                }
-            }
+        }
 
-            int distance = abs(current - prev);
-            if (distance > 3)
-            {
-                isSafe = false;
-                break;
-            } 
+        if (reportIsSafe(subset))
+        {
+            return true;
+        }
+    } while(std::prev_permutation(bitmask.begin(), bitmask.end()));
+    return false;
+}
+
+void solve2(std::vector<std::string> lines)
+{
+    int safeSum = 0;
+
+    for(auto &line : lines)
+    {
+        std::istringstream ss(line);
+        std::vector<int> report;
+        int num;
+        int prev = -1;
+        while(ss >> num)
+        {
+            report.push_back(num);
+        }
+        bool isSafe = reportIsSafe(report);
+        if (!isSafe)
+        {
+            isSafe = checkSubsets(report);
         }
         safeSum += isSafe? 1 : 0;
     }
@@ -63,8 +118,8 @@ int main()
     {
         std::vector<std::string> lines = readLinesFromFile("./day02/input.txt");
         // handle input
-        solve1(lines);
-        //solve2(lines);
+        //solve1(lines);
+        solve2(lines);
     } 
     catch (const std::exception &ex)
     {
